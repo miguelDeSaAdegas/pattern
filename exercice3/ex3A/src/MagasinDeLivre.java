d810370f2fbefac2f9277d71490c5bcc4c9e1524
miguelDeSaAdegas <113899501+miguelDeSaAdegas@users.noreply.github.com>
2023-10-09 15:14:06 +0200

    Delete exercice3/ex3A directory

--- exercice3/ex3A/src/MagasinDeLivre.java
+++
@@ -1,9 +0,0 @@
-import java.util.HashMap;
-import java.util.Map;
-
-public class MagasinDeLivre extends Magasin{
-	@Override
-	public Product createProduct(String name, int anneeDeParution) {
-		return new Livre(name, anneeDeParution);
-	}
-}
